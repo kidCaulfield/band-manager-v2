86cbad7c9229f00f6e59e5a6131e34ba18e62f25
Wes Coderre <wesley.coderre@gmail.com>
2019-04-26 14:32:02 -0700

    googlePlaceAPI nearbySearch controller returns places details

--- controllers/googlePlacesAPI.js
+++ controllers/googlePlacesAPI.js
@@ -4,12 +4,15 @@ const key = require('../config');
 module.exports = {
   async nearbySearch(req, res, next) {
     const { name, geo} = req.body;
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=49.2599155,-123.0964901&radius=50&name=biltmore_cabaret&keyword=biltmore_cabaret&key=${key.googleAPIKey}`,
-                                {
-                                  headers: {'Content-Type': 'aplication.json'}
-                                })
-    console.log('response: ', response.data);
-    res.status(200).json(response.data);
+    const searchName = name.split(" ").map(word => word.toLowerCase()).join("_").trim()
+    console.log('searchName: ', searchName);
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${geo.latitude},${geo.longitude}&radius=50&name=${searchName}&keyword=${searchName}&key=${key.googleAPIKey}`,
+                          {headers: {'Content-Type': 'aplication.json'}})
+    const placeDetails = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${response.data.results[0].place_id}&fields=name,rating,website,formatted_address,vicinity,international_phone_number&key=${key.googleAPIKey}`,
+                          {headers: {'Content-Type': 'aplication.json'}})
+ 
+    console.log('response2: ', placeDetails.data);
+    res.status(200).json(placeDetails.data);
   }
 };
 
